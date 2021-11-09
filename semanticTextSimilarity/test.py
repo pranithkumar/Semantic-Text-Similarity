3194f63eeeca3113c7156661dedde292d3452ac3
varun <donivarun97@gmail.com>
2021-11-09 18:41:39 -0500

    sbert without glove updated test.py

--- semanticTextSimilarity/test.py
+++ semanticTextSimilarity/test.py
@@ -10,19 +10,25 @@ def eval_func(data_loader, model, device, embedding_layer):
 
     with torch.no_grad():
         for bi, d in tqdm(enumerate(data_loader), total=len(data_loader)):
-            ids = d["ids"]
-            token_type_ids = d["token_type_ids"]
-            mask = d["mask"]
+            data1_ids = d["data1_ids"]
+            data1_token_type_ids = d["data1_token_type_ids"]
+            data1_bert_mask = d["data1_bert_mask"]
+            data2_ids = d["data2_ids"]
+            data2_token_type_ids = d["data2_token_type_ids"]
+            data2_bert_mask = d["data2_bert_mask"]
             targets = d["targets"]
             data1_token_ids = d["data1_token_ids"]
             data2_token_ids = d["data2_token_ids"]
             data1_mask = d['data1_mask']
             data2_mask = d['data2_mask']
 
-            ids = ids.to(device, dtype=torch.long)
-            token_type_ids = token_type_ids.to(device, dtype=torch.long)
-            mask = mask.to(device, dtype=torch.long)
-            targets = targets.to(device, dtype=torch.long)
+            data1_ids = data1_ids.to(device, dtype=torch.long)
+            data1_token_type_ids = data1_token_type_ids.to(device, dtype=torch.long)
+            data1_bert_mask = data1_bert_mask.to(device, dtype=torch.long)
+            data2_ids = data2_ids.to(device, dtype=torch.long)
+            data2_token_type_ids = data2_token_type_ids.to(device, dtype=torch.long)
+            data2_bert_mask = data2_bert_mask.to(device, dtype=torch.long)
+            targets = targets.to(device, dtype=torch.float)
             data1_token_ids = data1_token_ids.to(device, dtype=torch.long)
             data2_token_ids = data2_token_ids.to(device, dtype=torch.long)
             data1_mask = data1_mask.to(device, dtype=torch.long)
@@ -36,9 +42,12 @@ def eval_func(data_loader, model, device, embedding_layer):
             av2 = torch.div(av2, torch.sum(data2_mask, dim=1).unsqueeze(-1))
 
             output = model(
-                ids=ids,
-                mask = mask,
-                token_type_ids = token_type_ids,
+                data1_ids=data1_ids,
+                data1_mask=data1_bert_mask,
+                data1_token_type_ids=data1_token_type_ids,
+                data2_ids=data2_ids,
+                data2_mask=data2_bert_mask,
+                data2_token_type_ids=data2_token_type_ids,
                 av=torch.cat((av1, av2), dim=1)
                 # data1_token_ids=data1_token_ids,
                 # data2_token_ids=data2_token_ids,
